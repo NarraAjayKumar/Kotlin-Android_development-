817476a3adeebd4b443683d67ccdc456e70d53e1
Ajaykumar <narraajaykumar4@gmail.com>
2025-11-30 18:58:11 +0530

    Add files via upload

---
+++ Decision Making/exp1.kt
@@ -0,0 +1,11 @@
+fun main() {
+    Ajay(-11)
+}
+fun Ajay(n: Int) {
+  if (n > 0) {
+println("Given number is Positive")
+    }
+    else {
+        println("Given number is negative or Zero")
+    }
+}
---
+++ Decision Making/exp2.kt
@@ -0,0 +1,14 @@
+fun main() {
+Ajay(0)
+}
+fun Ajay(n :Int) {
+    if(n>0) {
+        println("Given number is Postive")
+    }
+    else if(n==0) {
+        println("Given number is Zero")
+    }
+    else {
+        println("Given number is negative")
+    }
+}
